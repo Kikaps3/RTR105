e8616f60d70ef217a478a75d8136b4b1c653a71c
Kikaps3 <112944501+Kikaps3@users.noreply.github.com>
2022-09-27 10:01:28 +0300

    Create first.c

---
+++ first.c
@@ -0,0 +1,78 @@
+// vienas rindas komentars
+// vienas rindas komentars
+// vienas rindas komentars
+
+/* 
+komentāra paragrāfs vai bloks
+1.
+2.
+3.
+*/
+
+int main()
+ {
+ /*
+ // ar komentāra nokomentēts koda fragments //
+if(...)
+ {
+ ...
+ }
+ */
+ return 0;
+ }
+
+first.i
+// vienas rindas komentars
+// vienas rindas komentars
+// vienas rindas komentars
+
+/* 
+komentāra paragrāfs vai bloks
+1.
+2.
+3.
+*/
+
+int main()
+ {
+ /*
+ // ar komentāra nokomentēts koda fragments //
+if(...)
+ {
+ ...
+ }
+ */
+ return 0;
+ }
+
+first.s
+  GNU nano 2.9.3                                    first.s                                              
+
+        .file   "first.c"
+        .text
+        .globl  main
+        .type   main, @function
+main:
+.LFB0:
+        .cfi_startproc
+        pushq   %rbp
+        .cfi_def_cfa_offset 16
+        .cfi_offset 6, -16
+        movq    %rsp, %rbp
+        .cfi_def_cfa_register 6
+        movl    $0, %eax
+        popq    %rbp
+        .cfi_def_cfa 7, 8
+        ret
+        .cfi_endproc
+.LFE0:
+        .size   main, .-main
+        .ident  "GCC: (Ubuntu 7.4.0-1ubuntu1~18.04.1) 7.4.0"
+        .section        .note.GNU-stack,"",@progbits
+
+first.o
+
+^?ELF^B^A^A^@^@^@^@^@^@^@^@^@^A^@>^@^A^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^H^B^@^@^@^@^@^@^@^@^@^@@^@^$
+^@UH���^@^@^@^@]�^@GCC: (Ubuntu 7.4.0-1ubuntu1~18.04.1) 7.4.0^@^@^T^@^@^@^@^@^@^@^AzR^@^Ax^P^A^[^L^G^H�^$
+
+
